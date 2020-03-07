3b6017bb910e6680fe2986fdefcdae7641ed6191
Elena <elena.do1988@yandex.ru>
2020-03-07 17:08:15 +0300

    features are finalized

---
+++ src/blocks/components/features/__content/features__content.js
@@ -0,0 +1,13 @@
+modules.define('features__content', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declElem('features', 'content', {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
---
+++ src/blocks/components/features/__footer/features__footer.js
@@ -0,0 +1,13 @@
+modules.define('features__footer', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declElem('features', 'footer', {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
