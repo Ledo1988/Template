68907ab2f00c629ef64ce608360086f32faa1c8e
Elena <elena.do1988@yandex.ru>
2020-03-09 13:39:51 +0300

    name validation is added

--- src/blocks/modules/form-request/form-request.js
+++ src/blocks/modules/form-request/form-request.js
@@ -14,7 +14,7 @@ function validate(event) {
 		return false;
 	}
 
-	if (!userName.value) {
+	if (!userName.value || !isValidName(userName.value)) {
 		userName.closest(".form-request__input").classList.add("error");
 		return false;
 	}
@@ -27,7 +27,12 @@ function validate(event) {
 }
 
 function isValidEmail (emailAddress) {
-	var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
-	return pattern.test(emailAddress);
+	let patternEmail = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+	return patternEmail.test(emailAddress);
+}
+
+function isValidName (name) {
+	let patternName = new RegExp(/^([- A-Za-zа-яА-ЯёЁ]+)$/);
+	return patternName.test(name);
 }
 
